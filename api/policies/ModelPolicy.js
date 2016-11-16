c69843545156a06b6c2da3effde941d7b666d3f4
Alex Ough <alex.ough@sungardas.com>
2016-11-16 01:40:43 -0600

    fixed to check both user & role permissions

--- api/policies/ModelPolicy.js
+++ api/policies/ModelPolicy.js
@@ -16,28 +16,40 @@ module.exports = function ModelPolicy (req, res, next) {
 
     if (req.options.action === 'populate') {
       req.options.modelDefinition.findOne(req.param('parentid')).then(function(parentObj) {
-        return PermissionService.isAllowedToPerformAction(parentObj, req.user, PermissionService.getAction(req.options.action), ModelService.getTargetModelName(req), req.body)
-      }).then(function(canPerform) {
-        if (!canPerform) {
-          return res.badRequest({
-            error: 'Cannot perform action [' + action + '] on parent object'
-          });
-        }
-        else {
-          var assoc = _.find(req.options.associations,{alias: req.options.alias})
-          var modelName = assoc[assoc.type];
-          var model = sails.models[assoc[assoc.type]];
+        // find permission(s) on the parent model and check if there is any criteria that matches with parentObj
+        var options = _.defaults({
+          model: req.model,
+          user: req.user
+        }, req.options);
+        PermissionService.findModelPermissions(options).then(function (permissions) {
+          sails.log.silly('PermissionPolicy:', permissions.length, 'permissions grant',
+              PermissionService.getAction(options), 'on', req.model.name, 'for', req.user.username);
+          if (!permissions || permissions.length === 0) {
+            return res.badRequest({
+              error: 'Cannot perform action [' + req.options.action + '] on parent object'
+            });
+          }
+          else if (!PermissionService.hasPassingCriteria(parentObj, permissions, req.body)) {
+            return res.badRequest({
+              error: 'Cannot perform action [' + req.options.action + '] on parent object'
+            });
+          }
+          else {
+            var assoc = _.find(req.options.associations,{alias: req.options.alias})
+            var modelName = assoc[assoc.type];
+            var model = sails.models[assoc[assoc.type]];
 
-          var populateModelPromise = genModelPromise(modelName)
-          .then(function(model) {
-            if (!model) {
-              req.options.unknownPopulateModel = true;
-            }
-            req.options.populateModelDefinition = sails.models[model.identity];
-            req.populateModel = model;
-          });
-          next();
-        }
+            var populateModelPromise = genModelPromise(modelName)
+            .then(function(model) {
+              if (!model) {
+                req.options.unknownPopulateModel = true;
+              }
+              req.options.populateModelDefinition = sails.models[model.identity];
+              req.populateModel = model;
+            });
+            next();
+          }
+        });
       });
     }
     else {
